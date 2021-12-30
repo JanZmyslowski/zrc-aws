0952e81d5fc5a22b294f7133dcae4c49abb08e43
JanZmyslowski <37934721+JanZmyslowski@users.noreply.github.com>
2021-12-30 12:10:18 +0100

    init

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = { 
+    "accessKeyId": "ASIAYN4MGSALY32F4CVH",
+    "secretAccessKey": "L7szcLb6gjoBA5VRRDHwmWeOTF1pun+AqM56A8xk", 
+    "region": "us-east-1",
+    "sessionToken": "FwoGZXIvYXdzEGsaDGUhFM77YkU8SoUzgSLKAaIIhC+UG0alQEU8oAB7ej1CIMpGnz5SWX3IqDUanE44flob67tK+FMxCj6YVUe8RlBHxoO+OiepF774hC4VhmwrQGcjMJy1a6mvxw3Fhu7q7inFwSfUX/i3v7Z3x6PRo5Ba/FaD2TAc545lrUqKWiTTZAnIOsnmB1UmYPxsRJ8J0K9hdJ/Btq+w1W3HmT7GEzq5gANXmcTpcDkZIgfaKCLuJ2EzFZ9McwWna694PBV95HqjdJPDQ2nJcessdGUAj1lwKBosXma7Kwso06T7iwYyLUj88tgOVadm09BhHj+hi6sM0gBNlo0YYkK/9PxrVonzXaZKHm3qhPbQP3oXiA==" 
+}
+
+export default config;
