7099320a41eb0aa3ab4d170fa1904d5985766617
JanZmyslowski <37934721+JanZmyslowski@users.noreply.github.com>
2022-01-14 19:23:33 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    'accessKeyId': 'ASIAYN4MGSAL2ETRL7Y2',
-    'secretAccessKey': 'kJ+moF1FCLnGFIxXAyGiZAUAv3TfHeVObkY8sUIo',
+    'accessKeyId': 'ASIAYN4MGSALQZCPM5G4',
+    'secretAccessKey': 'KVuXoryXkdLlTjaResA1EEa4A9eLpE8hiOOf1vEw',
     'region': 'us-east-1',
-    'sessionToken': 'FwoGZXIvYXdzEIX//////////wEaDO7q3PK5t5BXQjkgNyLKAbT9IrDHDVuO+DLKlmifUod7ISr6HRvL15O0zqk6UbW3Bhd98nXJLrBIGR3YRT5sJtb4Z4XOtryzb6mCIz8+SO1lIrz04vR3WIu3bJm97gssvlec3ZWmD6fXYVunxsC5kWeAFMbD5wGvdnOCaalOC0xxtv+9niy383u+Jz78uaVO+4+Og6HTeShIfyDw0oPyQIgcPOoMbXTfAlmekeKnfJxWzbU4+IWguRVbRkgfOmssTghIUodkHwvXdhMIcXzHydpsSaWhdhnb7UwowL3SjgYyLaEgAP/GnugUx+J1mCntK0FLtRPKD8ikUUC6/zQvOP+wwSUdPq29nzwrBf5Rqg=='
+    'sessionToken': 'FwoGZXIvYXdzEHQaDCzZz2jmmTqxYAf1xyLKAYCs3cIu3aEkY8dwIGDLTMRFx4KKUiToyrnneV/8JqByZt+uOMFSM1kB5qamDKg+Vqwy55vQ+nABaZNuUKGkucwIi6UTP0P3vqcRqxnlp8Dmxuq5qzcxOY2TwiSos2+ItoJ+6JS1SoOyfiDRfh1xP0djl/g2tJCZeBhPh5cnDj4mDNwdylRhbobhDnjpMY2qxdwJe6VqJqC6EfNdyeLlpRD6LsbUN23EIAxFRcbgpZksG1H5udzvGoU7mAkQNtB8SnnrcayJTFt74uIooP6GjwYyLRA/zti7m3QpnmXiJeRP7XIfZGPAL/KZi2EycZOASH1oyTfZRtejW+XQnCAiNQ=='
 };
 
 export default config;
